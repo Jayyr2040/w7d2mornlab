e6344caec86dcfa57a65590e6c10e4ad368f7e30
jaytai2040 <jayyr2040@gmail.com>
2021-07-13 11:31:13 +0800

    lab1 update

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT;
 
 // 'DATA'
 // const fruits = ["apple", "banana", "pear"];
-const fruits = [
+/* const fruits = [
     {
       name: "apple",
       color: "red",
@@ -21,20 +21,87 @@ const fruits = [
       color: "yellow",
       readyToEat: true,
     },
-  ];
+  ]; */
+
+// const superheroes = ['Superman', 'Wonder Woman', 'Black Panther' ];
+
+const superheroes = [
+  {
+    name: "Superman",
+    powers: ['flight', 'invulnerability', 'x-ray vision']
+  },
+  {
+    name: "Wonder Woman",
+    powers: ['super strength', 'invulnerability']
+  },
+  {
+    name: "Black Panther",
+    powers: ['super strength', 'invulnerability', 'super agility']
+  },
+];
+
+const supervillians = [
+  {
+   name: "Lex Luthor", 
+   powers: ["super brain"]
+  },
+  {
+   name: "Ares", 
+   powers: ["shape shifting", "teleporting"]
+   },
+   {
+    name: "Killmonger", 
+    powers: ["ability to fit in Black Panther's suit", "internet agreement that he looks badass"]
+   }
+]
 
 // ROUTES
-// index => shows all the fruits
-app.get("/fruits/", (req, res) => {
-    res.send(fruits);
+// index => shows all the superheroes
+app.get("/superheroes", (req, res) => {
+    res.send(superheroes);
   });
+
+/*   app.get("/supervillians", (req, res) => {
+    res.send(supervillians);
+  }); */
 
 // show route => show 1 fruit
 // route uses :id => extract using req.params
-app.get("/fruits/:id", (req, res) => {
+/* app.get("/superheroes/:index", (req, res) => {
     const pos = req.params.id;
     res.send(fruits[pos]);
+  }); */
+
+/* app.get("/superheroes/:index", (req, res) => {
+    const pos = req.params.index;
+    res.send(superheroes[pos]);
+  }); */
+
+
+/*   app.get("/superheroes/:index", (req, res) => {
+    const pos = req.params.index;
+    res.send(`<h1>${superheroes[pos].name}</h1>
+        <ul>
+        <li>${superheroes[pos].powers[0]}
+        </li>
+        <li>${superheroes[pos].powers[1]}
+        </li>
+        <ul>
+    
+    `);
+  }); */
+
+  app.get("/superheroes/:index", (req, res) => {
+    const pos = req.params.index;
+    res.send(`<h1>${superheroes[pos].name}</h1>
+        <ul>
+        ${superheroes[pos].powers.map((item) => `<li>${item}</li>`).join(" ")}
+        <li>Nemesis: ${supervillians[pos].name}</li>
+        </ul>
+    
+    `);
   });
+
 
 app.listen(PORT, () => {
     console.log("app is running on port:", PORT);
